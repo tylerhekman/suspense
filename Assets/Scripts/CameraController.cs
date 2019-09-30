742c2751b28d4a53431d6d21f549791f8719b6d6
Tyler Hekman <tylerhekman@gmail.com>
2019-09-30 00:21:15 -0700

    v0.0.2.2 - add vertical mouse acceleration to smooth vertical camera movement

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -13,50 +13,56 @@ public class CameraController : MonoBehaviour
     public GameObject player;
     public CameraTargetController cameraTarget;
 
-    private float radius = 5.0f;
+    private float maxRadius = 5.0f;
     private float angle = 270;
     private float cameraHeight = 2.0f;
     private float backgroundHeightTracker = 0;
-    private float foo;
+    private float radius;
     private float rotationalSensitivity = 4.0f;
-    private float verticalSensitivity = 1.0f;
+    private float verticalSensitivityDown = 0.5f;
+    private float verticalSensitivityUp;
+
+    private float maxVerticalSensitivityUp = 16;
 
     void Start()
     {
-        print(cameraTarget.getCameraTargetOffset());
+        verticalSensitivityUp = verticalSensitivityDown * 2.0f;
+        maxVerticalSensitivityUp = 0;
     }
 
     void Update()
     {
         angle -= Input.GetAxis("Mouse X") * rotationalSensitivity;
 
-        if(cameraHeight == 0.1f && backgroundHeightTracker <= 0)
+        float dyUp = Mathf.Clamp((Input.GetAxis("Mouse Y") * Mathf.Abs(Input.GetAxis("Mouse Y"))) * verticalSensitivityUp, -32.0f, 32.0f);
+
+        if (cameraHeight == 0.1f && backgroundHeightTracker <= 0)
         {
-            backgroundHeightTracker -= Input.GetAxis("Mouse Y") * verticalSensitivity;
+            backgroundHeightTracker -= dyUp;
             if(backgroundHeightTracker < -20)
             {
                 backgroundHeightTracker = -20;
             } else
             {
-                cameraTarget.cameraTargetHeightModifier += Input.GetAxis("Mouse Y");
+                cameraTarget.cameraTargetHeightModifier += dyUp;
             }
         }
         else
         {
             backgroundHeightTracker = 0;
             cameraTarget.cameraTargetHeightModifier = .4f;
-            cameraHeight -= Input.GetAxis("Mouse Y") * verticalSensitivity;
+            cameraHeight -= Input.GetAxis("Mouse Y") * verticalSensitivityDown;
             cameraHeight = Mathf.Clamp(cameraHeight, 0.1f, 4.5f);
-            foo = Mathf.Clamp(Mathf.Sqrt(Mathf.Pow(radius, 2) - Mathf.Pow(cameraHeight, 2)), 2, 5);
+            radius = Mathf.Clamp(Mathf.Sqrt(Mathf.Pow(maxRadius, 2) - Mathf.Pow(cameraHeight, 2)), 2, 5);
         }
     }
 
     void LateUpdate()
     {
         transform.position = new Vector3(
-            player.transform.position.x + (foo * Mathf.Cos(toRadian(angle))),
+            player.transform.position.x + (radius * Mathf.Cos(toRadian(angle))),
             player.transform.position.y + cameraHeight,
-            player.transform.position.z + (foo * Mathf.Sin(toRadian(angle)))
+            player.transform.position.z + (radius * Mathf.Sin(toRadian(angle)))
         );
         this.transform.LookAt(cameraTarget.transform);
     }
